221c680c248570ebfeb0019300e9612b76df6895
trejder <2903392+trejder@users.noreply.github.com>
2024-10-30 23:51:48 +0100

    Add second page to test translations between pages

--- data/translations.js
+++ data/translations.js
@@ -2,9 +2,11 @@ let translation = {
     pl:
     {
         "For details see translation.js file...": "Szczegółowe informacje znajdziesz w pliku translation.js...",
+        "Welcome to our game!": "Witaj w naszej grze!",
     },
     sx:
     {
         "For details see translation.js file...": "Detajlowe informacyje znojdziesz we zbioru translation.js...",
+        "Welcome to our game!": "Witej w naszym szpilu!",
     }
 };
