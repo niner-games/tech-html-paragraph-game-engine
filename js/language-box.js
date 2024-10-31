1ed9dbdfed570500855a3e287a77531f640a9fd4
trejder <2903392+trejder@users.noreply.github.com>
2024-10-31 13:40:23 +0100

    Change language select into series of buttons

--- js/language-box.js
+++ js/language-box.js
@@ -1,8 +1,4 @@
-const defaultLanguageOverlayFontSize = '14px';
-const defaultLanguageOverlayElementId = 'language-overlay';
-
 const LanguageBox = {
-    id: defaultLanguageOverlayElementId,
     languages: {
         'pl': 'polski',
         'en': 'English',
@@ -18,62 +14,33 @@ const LanguageBox = {
             return variable
         } else return def;
     },
-    
-    getId() {
-        return this.isSet(this.id, defaultLanguageOverlayElementId);
-    },
-
-    getLanguage() {
-        return window.localStorage.getItem("language") || AutoLoader.defaultLanguage;
-    },
 
     createOverlay: function() {
         let style = "ltr";
-        let overlay = document.createElement("div");
-        let select = document.createElement("select");
-        
-        overlay.id = this.getId();
-        
-        overlay.style.left = "0";
-        overlay.style.bottom = "0";
-        overlay.style.padding = "3px";
-        overlay.style.position = "fixed";
-        overlay.style.margin = "0 0 7px 7px";
-        overlay.style.fontSize = defaultLanguageOverlayFontSize;
-        overlay.style.backgroundColor = "rgba(0, 0, 0, 0.1)";
-        overlay.style.border = "solid 2px rgba(0, 0, 0, 0.3)";
-        
+        let overlay = document.getElementById('language-overlay');
+
         for (let key in this.languages) {
             if (this.languages.hasOwnProperty(key)) {
-                let option = document.createElement("option");
+                let button = document.createElement("button");
                 
                 style = (key === "iw") ? "rtl" : style;
-                
-                option.value = key;
-                option.style.direction = style;
-                option.text = this.languages[key];
-                
-                select.appendChild(option);
+
+                button.value = key;
+                button.style.direction = style;
+                button.textContent = this.languages[key];
+
+                button.addEventListener('click', () => { this.setLanguage(key) });
+
+                overlay.appendChild(button);
             }
         }
         
-        select.id = "the-select";
-        select.value = this.getLanguage();
-        
-        select.style.fontSize = defaultLanguageOverlayFontSize;
-        
-        select.addEventListener("change", this.handleChange);
-
-        overlay.appendChild(select);
-        document.body.appendChild(overlay);
-
         return false;
     },
     
-    handleChange: function() {
-        let selected = this.options[this.selectedIndex].value;
-
+    setLanguage: function(selected = AutoLoader.defaultLanguage) {
         TranslationEngine.translateDocument(selected);
+
         window.localStorage.setItem("language", selected);
     }
 };
