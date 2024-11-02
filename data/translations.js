57b97f71d839a719d4b654846283725c4bceeab4
trejder <2903392+trejder@users.noreply.github.com>
2024-11-02 21:31:54 +0100

    Add main menu screen

--- data/translations.js
+++ data/translations.js
@@ -3,18 +3,26 @@ let translationsTable = {
     {
         "Customize the game to your needs.": "Dostosuj grę do swoich potrzeb.",
         "Welcome to our game!": "Witaj w naszej grze!",
-        "What now?": "Co teraz?",
-        "Base": "podstawowy",
-        "Negative": "negatyw",
-        "Back": "powrót",
+        "Made with ♥ in Upper Silesia!": "Stworzone z ♥ na Górnym Sląsku!",
+        "Base": "Podstawowy",
+        "Negative": "Negatyw",
+        "Back": "Powrót",
+        "Continue": "Kontynuuj",
+        "New Game": "Nowa gra",
+        "Settings": "Ustawienia",
+        "Main Menu": "Menu główne",
     },
     sx:
     {
         "Customize the game to your needs.": "Przipasuj szpil do swojich potrzeb.",
         "Welcome to our game!": "Witej w naszym szpilu!",
-        "What now?": "Co terŏz?",
-        "Base": "bazowy",
-        "Negative": "ôdwrōcōny",
+        "Made with ♥ in Upper Silesia!": "Stworzōne z ♥ na Gōrnym Ślōnsku!",
+        "Base": "Bazowy",
+        "Negative": "Ôdwrōcōny",
         "Back": "Nazŏd",
+        "Continue": "Kōntynuuj",
+        "New Game": "Nowy szpil",
+        "Settings": "Ustawiyniŏ",
+        "Main Menu": "Myni głowne",
     }
 };
