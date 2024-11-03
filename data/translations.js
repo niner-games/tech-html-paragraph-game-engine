aed1d921578bcd584b4e89803088b18c789f4518
trejder <2903392+trejder@users.noreply.github.com>
2024-11-04 00:59:23 +0100

    Add warning to start a new game when old one is in progress

--- data/translations.js
+++ data/translations.js
@@ -12,6 +12,10 @@ let Translations = {
         "New Game": "Nowa gra",
         "Options": "Opcje",
         "(some translations are AI-generated and may not be very accurate)": "(niektóre tłumaczenia stworzyła sztuczna inteligencja i mogą one nie być zbyt trafne)",
+        "You have a an active game in progress!": "Masz aktywną grę w toku!",
+        "Starting a new game now will cause you to loose everything,<br />you have played so far. Are you sure you want to continue?": "Rozpoczynąjąc nową grę teraz, stracisz wszystko,<br />co osiągnięto do tej pory. Czy na pewno chcesz kontynuować?",
+        "Start a new game!": "Zacznij nową grę!",
+        "No, let's cancel it": "Nie, anulujmy to",
     },
     sx:
     {
@@ -27,5 +31,9 @@ let Translations = {
         "Options": "Ôpcyje",
         "Menu": "Myni",
         "(some translations are AI-generated and may not be very accurate)": "(niykere przekładu stworziła sztucznŏ inteligyncyjŏ i mogōm ône niy być za wiela trŏfne)",
+        "You have a an active game in progress!": "Mŏsz aktywny szpil w ciōngu!",
+        "Starting a new game now will cause you to loose everything,<br />you have played so far. Are you sure you want to continue?": "Rozpoczynąjąc nowy szpil terŏz, stracisz wszyjsko,<br />co ôsiōngniynto do tyj poryje. Eli na zicher chcesz kōntynuować?",
+        "Start a new game!": "Zacznij nowy szpil!",
+        "No, let's cancel it": "Niy, pociepmy to",
     }
 };
